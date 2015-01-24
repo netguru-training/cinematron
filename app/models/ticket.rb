674d34acc2538c4aad4d02dcf669aaa1b079aa08
Magdalena Poprawa <magdalena.poprawa@naturaily.com>
2015-01-24 16:19:36 +0100

    ticket types

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,6 @@
 class Ticket < ActiveRecord::Base
+  TICKET_TYPE = %w(child2D child3D adult2D adult3D)
+
+  validates :ticket_type, presence: true
+  validates :ticket_type, inclusion: { in: TICKET_TYPE }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movie = Movie.create({ title: 'Matrix', description: '01001100' })
+#ticket = Ticket.create({ })
