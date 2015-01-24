a7cc87f14474c5b9f31003e065ad276141153852
Oskar Lakner <oskar.lakner@netguru.pl>
2015-01-24 15:44:14 +0100

    Add association to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,7 @@
 class Reservation < ActiveRecord::Base
+  has_many :reservation_details
+  has_many :tickets, throught: :reservation_details
+  has_many :seats, throught: :reservation_details
 
   validates :fullname, :email, :movie_id, presence: true
-
 end
